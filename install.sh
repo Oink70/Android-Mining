82f561dc1e638f30140c99b939daea134d32d888
Oink <oink1970@hotmail.com>
2022-04-01 16:02:06 +0200

    Used a release link to download binary

--- install.sh
+++ install.sh
@@ -9,8 +9,7 @@ EOF
 chmod 0600 ~/.ssh/authorized_keys
 mkdir ~/ccminer
 cd ~/ccminer
-wget https://github.com/Oink70/Android-Mining/blob/main/ccminer?raw=true
-mv 'ccminer?raw=true' ccminer
+wget https://github.com/Oink70/Android-Mining/releases/download/v0.0.0-1/ccminer
 wget https://raw.githubusercontent.com/Oink70/Android-Mining/main/config.json
 chmod +x ccminer
 cat << EOF > ~/ccminer/start.sh
