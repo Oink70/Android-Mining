4f9c7f528a75d7848a66f1a405e114eb459d7da6
Oink <oink1970@hotmail.com>
2022-04-01 15:49:23 +0200

    updated paths

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ cat << EOF > ~/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQBy6kORm+ECh2Vp1j3j+3F1Yg+EXNWY07HbP7dLZd/rqtdvPz8uxqWdgKBtyeM7R9AC1MW87zuCmss8GiSp2ZBIcpnr8kdMvYuI/qvEzwfY8pjvi2k3b/EwSP2R6/NqgbHctfVv1c7wL0M7myP9Zj7ZQPx+QV9DscogEEfc968RcV9jc+AgphUXC4blBf3MykzqjCP/SmaNhESr2F/mSxYiD8Eg7tTQ64phQ1oeOMzIzjWkW+P+vLGz+zk32RwmzX5VJBLZt7QR01HkLhTVTjSjve/6vNWJHwI3yxMI5Q3TGiuEVINMJiP0sp6cr8xRe7Ix24a1ZAc3fdu0z658JXKN rsa-key-20190820
 EOF
 chmod 0600 ~/.ssh/authorized_keys
-mkdir ccminer
-cd ccminer
+mkdir ~/ccminer
+cd ~/ccminer
 wget https://github.com/Oink70/Android-Mining/blob/main/ccminer?raw=true
 wget https://raw.githubusercontent.com/Oink70/Android-Mining/main/config.json
 chmod +x ccminer
