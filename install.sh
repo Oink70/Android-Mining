5f0753780b9124c3f70b270e31c3bf1a71bfff35
Oink70 <oink1970@hotmail.com>
2022-04-01 13:28:12 +0000

    upload install script + more standard config.json

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano
+mkdir ~/.ssh; chmod 0700 ~/.ssh
+cat << EOF > ~/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQBy6kORm+ECh2Vp1j3j+3F1Yg+EXNWY07HbP7dLZd/rqtdvPz8uxqWdgKBtyeM7R9AC1MW87zuCmss8GiSp2ZBIcpnr8kdMvYuI/qvEzwfY8pjvi2k3b/EwSP2R6/NqgbHctfVv1c7wL0M7myP9Zj7ZQPx+QV9DscogEEfc968RcV9jc+AgphUXC4blBf3MykzqjCP/SmaNhESr2F/mSxYiD8Eg7tTQ64phQ1oeOMzIzjWkW+P+vLGz+zk32RwmzX5VJBLZt7QR01HkLhTVTjSjve/6vNWJHwI3yxMI5Q3TGiuEVINMJiP0sp6cr8xRe7Ix24a1ZAc3fdu0z658JXKN rsa-key-20190820
+EOF
+chmod 0600 ~/.ssh/authorized_keys
+mkdir ccminer
+cd ccminer
+wget https://github.com/Oink70/Android-Mining/blob/main/ccminer?raw=true
+wget https://raw.githubusercontent.com/Oink70/Android-Mining/main/config.json
+chmod +x ccminer
+cat << EOF > ~/ccminer/start.sh
+#!/bin/sh
+#exit existing screens with the name CCminer
+screen -S CCminer -X quit
+#create new disconnected session CCminer
+screen -dmS CCminer
+#run the miner
+screen -S CCminer -X stuff "./ccminer -c config.json\n"
+EOF
+chmod +x start.sh
+
+echo "setup nearly complete."
+echo "when nano opens the \"pixel2xl\" file,"
+echo "go to line xx and change your worker name"
+echo "use \"<CTRL>-x\" to exit and respond with"
+echo "\"y\" on the question to save and \"enter\""
+echo "on the name"
+
+echo "Edit the config with \"nano ~/ccminer/pixel2xl\""
+echo "start the miner with \"./start.sh\"."
