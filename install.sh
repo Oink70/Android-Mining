3b8beac6a5f8df43e65ce70cb9dae70302ce84eb
Oink <oink1970@hotmail.com>
2023-05-22 11:35:08 +0200

    Update install.sh
    Switch to downloading the non-optimized, but PBaaS enabled miner.

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ EOF
 chmod 0600 ~/.ssh/authorized_keys
 mkdir ~/ccminer
 cd ~/ccminer
-wget https://github.com/Oink70/Android-Mining/releases/download/v0.0.0-1/ccminer
+wget https://github.com/Oink70/Android-Mining/releases/download/v0.0.0-2/ccminer
 wget https://raw.githubusercontent.com/Oink70/Android-Mining/main/config.json
 chmod +x ccminer
 cat << EOF > ~/ccminer/start.sh
