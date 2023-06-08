adbb5f82870df684df9490cf109a8e95afec8bc7
Oink <oink1970@hotmail.com>
2023-06-08 19:19:11 +0200

    Update install.sh
    n00b instructions included

--- install.sh
+++ install.sh
@@ -19,13 +19,20 @@ chmod +x ccminer
 cat << EOF > ~/ccminer/start.sh
 #!/bin/sh
 #exit existing screens with the name CCminer
-screen -S CCminer -X quit
+screen -S CCminer -X quit 1>/dev/null 2>&1
 #wipe any existing (dead) screens)
-screen -wipe
+screen -wipe 1>/dev/null 2>&1
 #create new disconnected session CCminer
-screen -dmS CCminer
+screen -dmS CCminer 1>/dev/null 2>&1
 #run the miner
-screen -S CCminer -X stuff "~/ccminer/ccminer -c ~/ccminer/config.json\n"
+screen -S CCminer -X stuff "~/ccminer/ccminer -c ~/ccminer/config.json\n" 1>/dev/null 2>&1
+printf "\nMining started.\n"
+printf "---------------\n"
+printf "\nManual:\n"
+printf "start: ~/.ccminer/start.sh\n"
+printf "stop: screen -X -S CCminer quit\n"
+printf "\nmonitor mining: screen -x CCminer\n"
+printf "exit monitor: 'CTRL-a' followed by 'd'\n\n"
 EOF
 chmod +x start.sh
 
