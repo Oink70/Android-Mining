1e61a4fe3cd713a9f073d41e2b5474f2d6b87b37
Oink <oink1970@hotmail.com>
2023-06-16 17:20:45 +0200

    Update install.sh
    printf generated errors caused by poor formatting.

--- install.sh
+++ install.sh
@@ -26,12 +26,12 @@ screen -wipe 1>/dev/null 2>&1
 screen -dmS CCminer 1>/dev/null 2>&1
 #run the miner
 screen -S CCminer -X stuff "~/ccminer/ccminer -c ~/ccminer/config.json\n" 1>/dev/null 2>&1
-printf "\nMining started.\n"
-printf "---------------\n"
-printf "\nManual:\n"
-printf "start: ~/.ccminer/start.sh\n"
-printf "stop: screen -X -S CCminer quit\n"
-printf "\nmonitor mining: screen -x CCminer\n"
+printf '\nMining started.\n'
+printf '---------------\n'
+printf '\nManual:\n'
+printf 'start: ~/.ccminer/start.sh\n'
+printf 'stop: screen -X -S CCminer quit\n'
+printf '\nmonitor mining: screen -x CCminer\n'
 printf "exit monitor: 'CTRL-a' followed by 'd'\n\n"
 EOF
 chmod +x start.sh
