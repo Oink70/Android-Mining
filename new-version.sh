6d428760abadee6a2a06c5c4f3f2338e0c99b8bc
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-05 11:37:13 +0200

    Update new-version.sh

--- new-version.sh
+++ new-version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "\e[93m■■■■ pododabljam ubuntu ■■■■\e[0m"
+printf "\e[93m■■■■ pododabljam ubuntu ■■■■\e[0m"
 apt-get -y update
 apt-get -y upgrade
 # apt install -y libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano
@@ -8,9 +8,9 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 # sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano
 # naprej
-echo "\e[93m■■■■ zapiram miner - screen ■■■■\e[0m"
+printf "\e[93m■■■■ zapiram miner - screen ■■■■\e[0m"
 screen -X -S CCminer quit
-echo "\e[93m■■■■ prenašam novo verzijo ■■■■\e[0m"
+printf "\e[93m■■■■ prenašam novo verzijo ■■■■\e[0m"
 cd ~/ccminer
 # breišem starega
 rm -fr ccminer
@@ -18,7 +18,7 @@ rm -fr ccminer
 wget https://github.com/BLBMS/Android-Mining/releases/download/v3.8.3-4/ccminer-3.8.3-4_ARM
 mv ccminer-3.8.3-4_ARM ccminer
 chmod +x ccminer
-echo "\e[93m■■■■ posadabljam start.sh ■■■■\e[0m"
+printf "\e[93m■■■■ posadabljam start.sh ■■■■\e[0m"
 rm -f start.sh
 cat << EOF > ~/ccminer/start.sh
 #!/bin/sh
@@ -34,17 +34,17 @@ screen -wipe 1>/dev/null 2>&1
 screen -dmS CCminer 1>/dev/null 2>&1
 #run the miner
 screen -S CCminer -X stuff "~/ccminer/ccminer -c ~/ccminer/config.json\n" 1>/dev/null 2>&1
-printf '\nManual:\n'
-printf 'start       : ss  or  ~/.ccminer/start.sh\n'
-printf 'stop        : xx  or  screen -X -S CCminer quit\n'
-printf '\nmonitoring: rr  or  screen -x CCminer\n'
-printf "exit monitor: 'CTRL-a' followed by 'd'\n"
-printf "\nalias     : rr = show screen\n"
-printf "alias       : xx = kill screen\n"
+#printf '\nManual:\n'
+#printf 'start       : ss  or  ~/.ccminer/start.sh\n'
+#printf 'stop        : xx  or  screen -X -S CCminer quit\n'
+#printf '\nmonitoring: rr  or  screen -x CCminer\n'
+#printf "exit monitor: 'CTRL-a' followed by 'd'\n"
+printf "\nrr = show screen\n"
+printf "xx = kill screen\n"
 printf "alias       : sl = list screen\n"
 printf "alias       : ss = start ccminer\n"
 EOF
 chmod +x start.sh
-echo "\e[93m■■■■ posodobljeno + zagon start.sh ■■■■\e[0m"
+printf "\e[93m■■■■ posodobljeno + zagon start.sh ■■■■\e[0m"
 cd ~/ccminer
-./start.sh
+# test node ./start.sh
