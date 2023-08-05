4a97f9cbc103124aea56fe3ac2b6e91c377fcad8
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-05 11:32:06 +0200

    Update posodobi.sh

--- posodobi.sh
+++ posodobi.sh
@@ -2,8 +2,8 @@
 # POSODOBI fajl NOVA VERZIJA
 cd ~/ccminer
 rm -rf new-version.sh
-echo "\e[93m■■■■ prenašam: new-version.sh ■■■■\e[0m"
+printf "\e[93m■■■■ prenašam: new-version.sh ■■■■\e[0m\n"
 wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/new-version.sh
 chmod +x new-version.sh
-echo "\e[93m■■■■ štartam: new-version.sh ■■■■\e[0m"
+printf "\e[93m■■■■ štartam: new-version.sh ■■■■\e[0m\n"
 ./new-version.sh
