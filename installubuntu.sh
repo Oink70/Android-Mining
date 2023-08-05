8c0a8c90d2210dbec986fc3a18abfa023d603437
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-05 13:59:51 +0200

    Update installubuntu.sh

--- installubuntu.sh
+++ installubuntu.sh
@@ -1,55 +1,11 @@
 #!/bin/sh
+#exit
+printf "\n\e[93m!! start after installed Termux and UBUNTU is started !!\e[0m\n"
+read -p "Any key or CTRL-C to abort"
 
-exit
-
-
-# start after installed Termux and Termux.boot
-#
-echo  "1. Instaliraj termux & termux boot"
-echo  "https://f-droid.org/en/packages/com.termux/"
-echo  "https://f-droid.org/en/packages/com.termux.boot/"
-read -p "Press any key to continue or CTRL-C to abort"
-#
-# 2. Instaliraj ssh
-pkg update -y && pkg upgrade -y
-pkg install wget -y
-pkg install openssh -y
-pkg install net-tools -y
-sshd
-#
-echo "Če je error :"
-echo "#sshd: no hostkeys available -- exiting"
-read -p "Press any key to continue or CTRL-C to abort"
-#
-#Generate hostkeys za :
-ssh-keygen -A
-#
-# 3. Povezava ssh
-# Preveri user-ja :
-echo "Who I Am - mora biti root !!"
-whoami
-#
-read -p "Press any key to continue or CTRL-C to abort"
-#
-#Ustvari password :
-echo "Ustvari password"
-passwd root
-#
-echo "Preveri IP :"
-ifconfig
-
-read -p "vnesi zadnji IP: " mojIP
-
-#Poveži se z CMD :
-ssh $(printf root@192.168.100.)$mojIP -p 8022
-
-# install UBUNTU in Termux
-
-pkg update -y && pkg install curl proot tar -y && curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu/ubuntu.sh | bash
+printf "\n\e[93m■■■■ spreminajm ~/.bashrc ■■■■\e[0m\n"
 
 
 
-
-
-cat << EOF > ~/ccminer/start.sh
-#!/bin/sh
+#echo alias ss='~/ccminer/start.sh' >> ~/.bashrc
+#echo alias SS='ss' >> ~/.bashrc
