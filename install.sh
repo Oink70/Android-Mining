79bed9242dadfdba6f77eb3154c740080e730d84
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-05 15:12:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+printf "\n\e[93m■■■■ update/upgrade ■■■■\e[0m\n"
 #ne dela na vseh - zato 2x
 apt install sudo
 apt-get -y update
@@ -12,11 +13,29 @@ sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git scree
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+# ------ SSH
 mkdir ~/.ssh; chmod 0700 ~/.ssh
 cat << EOF > ~/.ssh/authorized_keys
 ssh-rsa AAAAB-i_dont_know_what
 EOF
 chmod 0600 ~/.ssh/authorized_keys
+# ------ bashrc
+printf "\n\e[93m■■■■ spreminjam ~/.bashrc ■■■■\e[0m\n"
+sed -i 's/^# force_color_prompt=yes*/force_color_prompt=yes/' ~/.bashrc
+sed -i 's/^#force_color_prompt=yes*/force_color_prompt=yes/' ~/.bashrc
+echo "" >> ~/.bashrc
+echo "#__________MOJE_________" >> ~/.bashrc
+echo 'bash -c "sleep 3 && ./ccminer/start.sh" && screen -r' >> ~/.bashrc
+echo "alias ss='~/ccminer/start.sh'" >> ~/.bashrc
+echo "alias xx='screen -X -S CCminer quit && screen -ls'" >> ~/.bashrc
+echo "alias sl='screen -ls'" >> ~/.bashrc
+echo "alias rr='screen -x CCminer'" >> ~/.bashrc
+echo "alias SS='ss'" >> ~/.bashrc
+echo "alias XX='xx'" >> ~/.bashrc
+echo "alias SL='sl'" >> ~/.bashrc
+echo "alias RR='rr'" >> ~/.bashrc
+printf "\n\e[93m■■■■ install CCminer ■■■■\e[0m\n"
+# ------ miner
 mkdir ~/ccminer
 cd ~/ccminer
 wget https://github.com/BLBMS/Android-Mining/releases/download/v3.8.3-4/ccminer-3.8.3-4_ARM
@@ -25,6 +44,7 @@ wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/posodobi.sh
 mv ccminer-3.8.3-4_ARM ccminer
 chmod +x ccminer
 chmod +x posodobi.sh
+# ------ start.sh
 cat << EOF > ~/ccminer/start.sh
 #!/bin/sh
 #wipe any existing (dead) screens from last session
