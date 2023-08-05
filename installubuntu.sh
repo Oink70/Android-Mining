3cbd9fb322ac135dbd76cb822eba10f270bbfd55
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-05 14:55:28 +0200

    Update installubuntu.sh

--- installubuntu.sh
+++ installubuntu.sh
@@ -8,18 +8,22 @@ printf "%s " "Enter to continue or CTRL-C to abort"
 read ans
 #read -rsn1 -p "Enter to continue or CTRL-C to abort" - dela v /bash
 
-printf "\n\e[93m■■■■ spreminjam ~/.bashrc  1  ■■■■\e[0m\n"
+printf "\n\e[93m■■■■ spreminjam ~/.bashrc ■■■■\e[0m\n"
 
-sed -i 's/^# force_color_prompt=yes*/force_color_prompt=yes/' ~/.bashrc1
+sed -i 's/^# force_color_prompt=yes*/force_color_prompt=yes/' ~/.bashrc
+sed -i 's/^#force_color_prompt=yes*/force_color_prompt=yes/' ~/.bashrc
 
-echo '#' >> ~/.bashrc1
-echo '#__________MOJE_________' >> ~/.bashrc1
-echo 'bash -c "sleep 3 && ./ccminer/start.sh" && screen -r' >> ~/.bashrc1
-echo 'alias rr='screen -x CCminer'' >> ~/.bashrc1
-echo 'alias xx='screen -X -S CCminer quit && screen -ls'' >> ~/.bashrc1
-echo 'alias sl='screen -ls'' >> ~/.bashrc1
-echo 'alias ss='~/ccminer/start.sh'' >> ~/.bashrc1
-echo 'alias XX='xx'' >> ~/.bashrc1
-echo 'alias RR='rr'' >> ~/.bashrc1
-echo 'alias SL='sl'' >> ~/.bashrc1
-echo 'alias SS='ss'' >> ~/.bashrc1
+echo "" >> ~/.bashrc
+echo "#__________MOJE_________" >> ~/.bashrc
+echo 'bash -c "sleep 3 && ./ccminer/start.sh" && screen -r' >> ~/.bashrc
+echo "alias ss='~/ccminer/start.sh'" >> ~/.bashrc
+echo "alias xx='screen -X -S CCminer quit && screen -ls'" >> ~/.bashrc
+echo "alias sl='screen -ls'" >> ~/.bashrc
+echo "alias rr='screen -x CCminer'" >> ~/.bashrc
+echo "alias SS='ss'" >> ~/.bashrc
+echo "alias XX='xx'" >> ~/.bashrc
+echo "alias SL='sl'" >> ~/.bashrc
+echo "alias RR='rr'" >> ~/.bashrc
+
+printf "\n\e[93m■■■■ spreminjeno ■■■■\e[0m\n"
+
