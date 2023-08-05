e1209cfbf83f28fdf20f3406a4cb5caab6a1a5b0
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-05 09:50:12 +0200

    Create posodobi.sh

---
+++ posodobi.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# POSODOBI fajl NOVA VERZIJA
+cd ~/
+rm -rf new-version.sh
+wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/new-version.sh
+chmod +x new-version.sh
+./new-version.sh
