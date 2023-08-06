f2e24eaddcf35cba752de064c58742fd53313d49
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-06 12:04:23 +0200

    Update backto33.sh

--- backto33.sh
+++ backto33.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+#  wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/backto33.sh && chmod +x backto33.sh && ./backto33.sh
 
 screen -X -S CCminer quit
 #cp ~/ccminer/config.json ~/
@@ -8,4 +9,4 @@ mv ccminer ccminer3-4
 wget https://github.com/BLBMS/Android-Mining/releases/download/v3.8.3-3/ccminer-3.8.3-3_ARM
 mv ccminer-3.8.3-3_ARM ccminer
 chmod +x ccminer
-screen -S CCminer -X stuff "~/ccminer/ccminer -c ~/ccminer/config.json\n" 1>/dev/null 2>&1
+ss
