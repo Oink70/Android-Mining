93f2f5ba1c60f1ae7a65e90d3307e8ca1a2aae36
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-07 23:26:50 +0200

    Update pool-verus.sh

--- pool-verus.sh
+++ pool-verus.sh
@@ -2,15 +2,17 @@
 
 #  wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/pool-verus.sh && chmod +x pool-verus.sh && ./pool-verus.sh
 
-# Prenese datotetko s pool VERUS
-cd ~/ccminer/
-wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/pool-verus.json
-
 # Ime vhodne datoteke
 input_file="config.json"
 
 # Ime izhodne datoteke
 output_file="config-verus.json"
+
+printf "\n\e[93m■■■ new pool: VERUS.IO ■■■\e[0m"
+
+# Prenese datotetko s pool VERUS
+cd ~/ccminer/
+wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/$output_file
 
 # Iskanje vrstice, ki se začne z "user:" in kopiranje do konca datoteke
 sed -n '/^"user":/,$p' "$input_file" >> "$output_file"
@@ -21,6 +23,6 @@ echo "Nastavitve minerja so bile dodane na konec $output_file."
 mv -rf config.json config-luck.json
 
 # Nastavi novo in reset minerja
-xx
+#xx
 cp -rf config-verus.json config.json
-ss
+#ss
