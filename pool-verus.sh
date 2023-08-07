ed96090ed1cd367eaab0fbda1c4169ebc838aaae
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-08 00:19:23 +0200

    Update pool-verus.sh

--- pool-verus.sh
+++ pool-verus.sh
@@ -2,27 +2,41 @@
 
 #  wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/pool-verus.sh && chmod +x pool-verus.sh && ./pool-verus.sh
 
-# Ime vhodne datoteke
-input_file="config.json"
-
-# Ime izhodne datoteke
-output_file="config-verus.json"
-
-printf "\n\e[93m■■■ new pool: VERUS.IO ■■■\e[0m"
-
 # Prenese datotetko s pool VERUS
 cd ~/ccminer/
-wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/$output_file
+new_file="config-verus.json"
+wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/$new_file
 
-# Iskanje vrstice, ki se začne z "user:" in kopiranje do konca datoteke
-sed -n '/^"user":/,$p' "$input_file" >> "$output_file"
+pool_name="${new_file#*config-}"
+pool_name="${pool_name%.json}"
 
-echo "Nastavitve minerja so bile dodane na konec $output_file."
+printf "\n\e[93m■■■ new pool: $pool_name ■■■\e[0m\n"
 
-# Shrani obst. v pool-luck
-mv -rf config.json config-luck.json
+# Ime datoteke s staro vsebino
+file="config.json"
 
-# Nastavi novo in reset minerja
-#xx
-cp -rf config-verus.json config.json
-#ss
+# Uporaba grep za iskanje ustreznega niza in izpis vsebine
+content=$(grep -o '4wc\..*",' "$file" | sed 's/4wc\.//;s/",//')
+
+# Preverimo, ali je vsebina enaka nizu "BLB"
+if [ "$content" = "BLB" ]; then
+    echo "Vsebina je enaka nizu BLB."
+    printf "\n\e[93m IME DELAVCA: \e[0m"
+    read new_content
+elif [ -z "$content" ]; then
+    echo "Napaka: Ustrezen niz ni bil najden v datoteki $file."
+    printf "\n\e[93m IME DELAVCA: \e[0m"
+    read new_content
+else
+    echo "Najden delavec je: $content"
+    new_content=$content
+fi
+
+# Zapri vse screene
+#screen -ls | grep -o '[0-9]\+\.' | awk '{print $1}' | xargs -I {} screen -X -S {} quit
+
+# Iskanje niza "BLB" in zamenjava z vsebino iz datoteke .ww
+sed -i "s/BLB/$new_content/" "$old_content_file"
+
+# Zažene miner
+#~/ccminer/start.sh
