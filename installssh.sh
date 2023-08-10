e1e19577a3c54573e263d1f4a61cc5b1fa4dcd6d
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-10 19:34:50 +0200

    Update installssh.sh

--- installssh.sh
+++ installssh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#  cd ~/ && rm -f installssh.sh && wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/installssh.sh && chmod +x installssh.sh && ./installssh.sh
+#  cd ~/ && rm -f ~/installssh.sh && wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/installssh.sh && chmod +x installssh.sh && ~/installssh.sh
 
 printf "\n\e[93m■■■■ update to blb ssh ■■■■\e[0m\n"
 # ------ SSH
@@ -14,4 +14,4 @@ ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAe7mHnisRNUXZ8u5AaeKxm7/ixbaacLWk6S6bpqlEom
 EOF
 chmod 0600 ~/.ssh/authorized_keys
 printf "\n\e[93m■■■■ done ■■■■\e[0m\n"
-rm -f installssh.sh
+rm -f ~/installssh.sh
