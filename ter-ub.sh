5a0ab165980d0061f98b7be63b433e05213395aa
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-10 19:44:14 +0200

    Create ter-ub.sh

---
+++ ter-ub.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#  cd ~/ && rm -f ~/ter-ub.sh && wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/ter-ub.sh && chmod +x ter-ub.sh && ~/ter-ub.sh
+
+printf "\n\e[93m■■■■ test ■■■■\e[0m\n"
+# ------ 
+./start-ubuntu.sh
+sleep 10
+printf "\n\e[93m■■■■ ubuntu ■■■■\e[0m\n"
+ls
