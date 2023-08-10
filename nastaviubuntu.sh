7f2ec843de4d3e4c5b588d74871d67dc16ddb1fe
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-11 00:30:56 +0200

    Update nastaviubuntu.sh

--- nastaviubuntu.sh
+++ nastaviubuntu.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+printf "\n\e[93m■■■ nastavljam v UBUNTU ■■■\e[0m\n"
 rm -f ~/new-version.sh
 rm -f ~/posodobi.sh
 rm -f ~/ccminer/new-version.sh
 rm -f ~/ccminer/posodobi.sh
 cd ~/ && wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/posodobi.sh && chmod +x posodobi.sh
 cd ~/ && rm -f pool-verus.sh && wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/pool-verus.sh && chmod +x pool-verus.sh
+cd ~/ && rm -f pool-luck.sh && wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/pool-luck.sh && chmod +x pool-luck.sh
