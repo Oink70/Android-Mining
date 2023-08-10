4334bb5fe65b776ec687356561174871d1b6ab1b
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-10 19:48:59 +0200

    Update ter-ub.sh

--- ter-ub.sh
+++ ter-ub.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 #  cd ~/ && rm -f ~/ter-ub.sh && wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/ter-ub.sh && chmod +x ter-ub.sh && ~/ter-ub.sh
 
 printf "\n\e[93m■■■■ test ■■■■\e[0m\n"
 # ------ 
 ./start-ubuntu.sh
-#sleep 10
+sleep 15
 printf "\n\e[93m■■■■ ubuntu ■■■■\e[0m\n"
 sleep 5
 ls
