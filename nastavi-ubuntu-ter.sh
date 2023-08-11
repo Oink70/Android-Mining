2b83fb753592eae54ce2aa3fefeb982ed7f884fe
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-11 17:12:11 +0200

    Update nastavi-ubuntu-ter.sh

--- nastavi-ubuntu-ter.sh
+++ nastavi-ubuntu-ter.sh
@@ -2,4 +2,4 @@
 printf "\n\e[93m■■■ TERMUX: nastavljam v UBUNTU ■■■\e[0m\n"
 # ./start-ubuntu.sh ./nastavi-ubuntu.sh
 ./start-ubuntu.sh 'cd ~/ && rm -f nastavi-ubuntu.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/nastavi-ubuntu.sh && chmod +x nastavi-ubuntu.sh && ./nastavi-ubuntu.sh'
-printf "\n\e[93m■■■ TERMUX: nastavljeno ■■■\e[0m\n"
+printf "\n\e[93m■■■ TERMUX: nastavljeno v UBUNTU ■■■\e[0m\n"
