6025255a42538d3e36c978d2b07fc8c680234450
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-11 11:42:34 +0200

    Create restart-miner.sh

---
+++ restart-miner.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+printf "\n\e[93m■■■ UBUNTU: restart miner ■■■\e[0m\n"
+# ustavi vse screene in ponovno zažene miner
+./start-ubuntu.sh 'cd ~/ && screen -ls | grep -o '[0-9]\+\.' | awk '{print $1}' | xargs -I {} screen -X -S {} quit && ~/ccminer/start.sh'
