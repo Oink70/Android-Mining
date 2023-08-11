5e3a34954213b5364108787aedabd53422f85d57
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-11 16:56:16 +0200

    Update posodobi.sh

--- posodobi.sh
+++ posodobi.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 #
-# cd ~/ && rm -f posodobi.sh && wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/posodobi.sh && chmod +x posodobi.sh
-#
 # POSODOBI fajl NOVA VERZIJA
 cd ~/
 rm -f new-version.sh
 printf "\e[93m■■■■ prenašam: new-version.sh ■■■■\e[0m\n"
-wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/new-version.sh
+wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/new-version.sh
 chmod +x new-version.sh
 printf "\e[93m■■■■ štartam: new-version.sh ■■■■\e[0m\n"
 ./new-version.sh
