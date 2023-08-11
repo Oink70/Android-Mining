f7d0ff7bd5bba567de4771020c2e203a0bf7af7c
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-11 14:22:55 +0200

    Update nastavi-termux.sh

--- nastavi-termux.sh
+++ nastavi-termux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# cd ~/ && rm -f nastavi-termux.sh && wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/nastavi-termux.sh && chmod +x nastavi-termux.sh && ./nastavi-termux.sh
+# cd ~/ && rm -f nastavi-termux.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/nastavi-termux.sh && chmod +x nastavi-termux.sh && ./nastavi-termux.sh
 
 printf "\n\e[93m■■■ nastavljam v TERMUX ■■■\e[0m\n"
 
@@ -9,8 +9,7 @@ printf "\n\e[93m■■■ nastavljam v TERMUX ■■■\e[0m\n"
 # vsebina xxxter
 # ./start-ubuntu.sh ./xxxub.sh
 
-cd ~/ && rm -f restart-miner-ter.sh && wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/restart-miner-ter.sh && chmod +x restart-miner-ter.sh
-cd ~/ && rm -f posodobi-ter.sh && wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/posodobi-ter.sh && chmod +x posodobi-ter.sh
-cd ~/ && rm -f pool-verus-ter.sh && wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/pool-verus-ter.sh && chmod +x pool-verus-ter.sh
-cd ~/ && rm -f pool-luck-ter.sh && wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/pool-luck-ter.sh && chmod +x pool-luck-ter.sh
-cd ~/ && rm -f nastavi-ubuntu-ter.sh && wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/nastavi-ubuntu-ter.sh && chmod +x nastavi-ubuntu-ter.sh
+cd ~/ && rm -f restart-miner-ter.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/restart-miner-ter.sh && chmod +x restart-miner-ter.sh
+cd ~/ && rm -f posodobi-ter.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/posodobi-ter.sh && chmod +x posodobi-ter.sh
+cd ~/ && rm -f pool-verus-ter.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/pool-verus-ter.sh && chmod +x pool-verus-ter.sh
+cd ~/ && rm -f pool-luck-ter.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/pool-luck-ter.sh && chmod +x pool-luck-ter.sh
