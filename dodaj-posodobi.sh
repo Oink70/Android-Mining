7f4dc9d13a3b42fba632fc278b92674d2da26415
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-11 14:25:04 +0200

    Update dodaj-posodobi.sh

--- dodaj-posodobi.sh
+++ dodaj-posodobi.sh
@@ -1,3 +1,3 @@
 ____ kopiraj v telefon
 
-cd ~/ccminer && rm -rf posodobi.sh && wget https://raw.githubusercontent.com/BLBMS/Android-Mining/main/posodobi.sh && chmod +x posodobi.sh
+cd ~/ccminer && rm -rf posodobi.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/posodobi.sh && chmod +x posodobi.sh
