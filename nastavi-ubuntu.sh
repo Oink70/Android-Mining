a4958078bd9cfcbedbdc8ef5ae4d69eed7df5004
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-12 23:12:22 +0200

    Update nastavi-ubuntu.sh

--- nastavi-ubuntu.sh
+++ nastavi-ubuntu.sh
@@ -6,3 +6,5 @@ cd ~/ && rm -f pool-verus.sh && wget -q https://raw.githubusercontent.com/BLBMS/
 cd ~/ && rm -f pool-luck.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/pool-luck.sh && chmod +x pool-luck.sh
 cd ~/ && rm -f install-in-ubuntu.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/install-in-ubuntu.sh && chmod +x install-in-ubuntu.sh
 printf "\n\e[93m■■■ nastavljeno v UBUNTU ■■■\e[0m\n"
+cd ~/ && ./install-in-ubuntu.sh
+printf "\n\e[93m■■■ inštalirano v UBUNTU ■■■\e[0m\n"
