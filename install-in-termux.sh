9190e3aea21aea0da0236ba165db17d7f65e7df5
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-12 22:58:50 +0200

    Update install-in-termux.sh

--- install-in-termux.sh
+++ install-in-termux.sh
@@ -7,40 +7,40 @@ printf "\n\e[93m■■■■ nastavitve v TERMUX ■■■■\e[0m\n"
 #printf "\n\e[93mupdate TERMUX\e[0m\n"
 # Instaliraj ssh - 6x YES
 #pkg update -y && pkg upgrade -y && pkg install wget -y && pkg install openssh -y && pkg install net-tools -y
-echo -e "\n\e[93mnastavljam SSH\e[0m\n"
+#echo -e "\n\e[93mnastavljam SSH\e[0m\n"
 # nastavi SSH
-sshd
+#sshd
 #  Če je error - to nekam vnesi ???:
 #  echo "#sshd: no hostkeys available -- exiting"
-ssh-keygen -A
+#ssh-keygen -A
 #  whoami
-my_name=$(whoami)
-echo "whoami=" $my_name
+#my_name=$(whoami)
+#echo "whoami=" $my_name
 #  Ustvari password
-passwd $my_name
+#passwd $my_name
 # Nastavi IP
-ifconfig_out=$(ifconfig)
-ip_line=$(echo "$ifconfig_out" | grep 'inet 192')
-phone_ip=$(echo "$ip_line" | cut -d'.' -f4 | cut -c1-3)
-echo "IP=" $phone_ip
-rm -f ~/*.ip
-cat << EOF > ~/$phone_ip.ip
-EOF
-echo $$phone_ip >> ~/$phone_ip.ip
+#ifconfig_out=$(ifconfig)
+#ip_line=$(echo "$ifconfig_out" | grep 'inet 192')
+#phone_ip=$(echo "$ip_line" | cut -d'.' -f4 | cut -c1-3)
+#echo "IP=" $phone_ip
+#rm -f ~/*.ip
+#cat << EOF > ~/$phone_ip.ip
+#EOF
+#echo $$phone_ip >> ~/$phone_ip.ip
 # Nastavi SSH
-ssh $my_name@192.168.100.$phone_ip -p 8022
-echo -e "\n\e[93mnastavljam key\e[0m\n"
-cd ~/
-rm -rf ~/.ssh/
-mkdir ~/.ssh
-chmod 0700 ~/.ssh
-cd ~/.ssh
-sleep 1
-cat << EOF > ~/.ssh/authorized_keys
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAe7mHnisRNUXZ8u5AaeKxm7/ixbaacLWk6S6bpqlEom blb@blb
-EOF
-chmod 0600 ~/.ssh/authorized_keys
-ls
+#ssh $my_name@192.168.100.$phone_ip -p 8022
+#echo -e "\n\e[93mnastavljam key\e[0m\n"
+#cd ~/
+#rm -rf ~/.ssh/
+#mkdir ~/.ssh
+#chmod 0700 ~/.ssh
+#cd ~/.ssh
+#sleep 1
+#cat << EOF > ~/.ssh/authorized_keys
+#ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAe7mHnisRNUXZ8u5AaeKxm7/ixbaacLWk6S6bpqlEom blb@blb
+#EOF
+#chmod 0600 ~/.ssh/authorized_keys
+#ls
 echo -e "\n\e[93minstall UBUNTU in Termux\e[0m\n"
 pkg update -y && pkg install curl proot tar -y && curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu/ubuntu.sh | bash
 echo -e "\n\e[93mnasrtavljam auto boot\e[0m\n"
