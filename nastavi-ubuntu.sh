2b2b6bd2ceb2e5bb610741e9dc8d9a44d0181711
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-12 23:11:26 +0200

    Update nastavi-ubuntu.sh

--- nastavi-ubuntu.sh
+++ nastavi-ubuntu.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 printf "\n\e[93m■■■ nastavljam v UBUNTU ■■■\e[0m\n"
-cd ~/ && rm -f ~/posodobi.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/posodobi.sh && chmod +x posodobi.sh
-cd ~/ && rm -f ~/restart-miner.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/restart-miner.sh && chmod +x restart-miner.sh
+cd ~/ && rm -f posodobi.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/posodobi.sh && chmod +x posodobi.sh
+cd ~/ && rm -f restart-miner.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/restart-miner.sh && chmod +x restart-miner.sh
 cd ~/ && rm -f pool-verus.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/pool-verus.sh && chmod +x pool-verus.sh
 cd ~/ && rm -f pool-luck.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/pool-luck.sh && chmod +x pool-luck.sh
+cd ~/ && rm -f install-in-ubuntu.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/install-in-ubuntu.sh && chmod +x install-in-ubuntu.sh
 printf "\n\e[93m■■■ nastavljeno v UBUNTU ■■■\e[0m\n"
