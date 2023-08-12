e2ab1ef60b672f56c4b7b173476f916949e1510c
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-12 22:49:15 +0200

    Update nastavi-termux.sh

--- nastavi-termux.sh
+++ nastavi-termux.sh
@@ -6,5 +6,6 @@ cd ~/ && rm -f posodobi-ter.sh && wget -q https://raw.githubusercontent.com/BLBM
 cd ~/ && rm -f pool-verus-ter.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/pool-verus-ter.sh && chmod +x pool-verus-ter.sh
 cd ~/ && rm -f pool-luck-ter.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/pool-luck-ter.sh && chmod +x pool-luck-ter.sh
 cd ~/ && rm -f nastavi-ubuntu-ter.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/nastavi-ubuntu-ter.sh && chmod +x nastavi-ubuntu-ter.sh
-cd ~/ && rm -f installssh.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/installssh.sh && chmod +x installssh.sh
+cd ~/ && rm -f install-in-termux.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/install-in-termux.sh && chmod +x install-in-termux.sh
 printf "\n\e[93m■■■ nastavljeno v TERMUX ■■■\e[0m\n"
+./install-in-termux.sh
