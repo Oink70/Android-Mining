cbcc5957818c3d745bd11ba5dda75ba994bee3a0
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-12 12:06:04 +0200

    Update add-wifi.sh

--- add-wifi.sh
+++ add-wifi.sh
@@ -1,2 +1,27 @@
 #!/bin/sh
+exit
+
+$$$$__UBUNTU - nej pravo
 sudo apt-get -y install network-manager
+
+sudo apt list network-manager
+
+sudo systemctl start NetworkManager
+
+nmcli device wifi list
+
+nmcli device wifi connect "SSID" password "geslo"
+
+$$$$__TERMUX
+
+$ pkg update
+
+??? pkg install networkmanager 
+
+$ wpa_supplicant
+The program wpa_supplicant is not installed. Install it by executing:
+pkg install wpa-supplicant, after running pkg install root-repo
+
+$ iwconfig
+The program iwconfig is not installed. Install it by executing:
+pkg install wireless-tools
