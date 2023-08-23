c8cfc997cde6cd3903deff5d79de9c1e207a92fb
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-23 22:42:48 +0200

    Update nastavi-ip-ter.sh

--- nastavi-ip-ter.sh
+++ nastavi-ip-ter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-printf "\n\e[93m■■■ TERMUX: pool-luck ■■■\e[0m\n"
+printf "\n\e[93m■■■ TERMUX: nastavi IP ■■■\e[0m\n"
 ./start-ubuntu.sh 'cd ~/ && rm -f ~/nastavi-ip.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/nastavi-ip.sh && chmod +x nastavi-ip.sh && ~/nastavi-ip.sh'
