60bcc105938b51aa0eaf96730ebb0d04e639e0a5
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-23 23:27:08 +0200

    Update restart-miner.sh

--- restart-miner.sh
+++ restart-miner.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 printf "\n\e[93m■■■ UBUNTU: restart miner ■■■\e[0m\n"
 # ustavi vse screene in ponovno zažene miner
 cd ~/ && screen -ls | grep -o '[0-9]\+\.' | awk '{print $1}' | xargs -I {} screen -X -S {} quit
