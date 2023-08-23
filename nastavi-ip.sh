973677814b934503a6730b7f6c79fcaf17dc4ef9
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-23 18:28:12 +0200

    Create nastavi-ip.sh

---
+++ nastavi-ip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#->  cd ~/ && rm -f ~/nastavi-ip.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/nastavi-ip.sh && chmod +x nastavi-ip.sh && ~/nastavi-ip.sh
+
+# Nastavi IP
+ifconfig_out=$(ifconfig)
+ip_line=$(echo "$ifconfig_out" | grep 'inet 192')
+phone_ip=$(echo "$ip_line" | cut -d'.' -f4 | cut -c1-3)
+echo "IP=" $phone_ip
+rm -f ~/*.ip
+cat << EOF > ~/$phone_ip.ip
+EOF
+echo $$phone_ip >> ~/$phone_ip.ip
