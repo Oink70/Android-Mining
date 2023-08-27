59097ceb977e6a51504417e720ed6d237dfda8f8
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-27 18:44:18 +0200

    Update nastavi-ubuntu.sh

--- nastavi-ubuntu.sh
+++ nastavi-ubuntu.sh
@@ -7,6 +7,7 @@ cd ~/ && rm -f posodobi.sh && wget -q https://raw.githubusercontent.com/BLBMS/An
 cd ~/ && rm -f restart-miner.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/restart-miner.sh && chmod +x restart-miner.sh
 cd ~/ && rm -f pool-verus.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/pool-verus.sh && chmod +x pool-verus.sh
 cd ~/ && rm -f pool-luck.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/pool-luck.sh && chmod +x pool-luck.sh
+cd ~/ && rm -f nastavi-ip.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/nastavi-ip.sh && chmod +x nastavi-ip.sh
 cd ~/ && rm -f install-in-ubuntu.sh && wget -q https://raw.githubusercontent.com/BLBMS/Android-Mining/main/install-in-ubuntu.sh && chmod +x install-in-ubuntu.sh
 printf "\n\e[93m■■■ kopiranono v UBUNTU ■■■\e[0m\n"
 cd ~/ && ./install-in-ubuntu.sh
