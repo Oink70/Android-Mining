4e6548c1193280b1ec0fa713c33225e39db8eaed
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-31 18:34:04 +0200

    Create active.sh

---
+++ active.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+active_count=0
+inactive_count=0
+inactive_ips=""
+echo "IP range: {110..118} 130 140 {150..154} {170..175}"
+#for ip in {110..120}; do  #test
+for ip in {110..118} 130 140 {150..154} {170..175}; do  #dober
+    echo -n -e "\r$ip "
+    if ping -c 1 -W 1 192.168.100.$ip &>/dev/null; then
+#        echo -e "\e[32mActiv  : $ip \e[0m"
+        ((active_count++))
+    else
+        echo -n "? "
+        sleep 1
+        if ping -c 3 -W 1 192.168.100.$ip &>/dev/null; then
+            echo -e -n "\b\b \b"
+            ((active_count++))
+        else
+            echo -e "\e[33mInactiv: $ip \e[0m"
+            ((inactive_count++))
+            inactive_ips+="$ip\n"
+        fi
+    fi
+done
+echo -n -e "\r--------------\n"
+echo -e "\e[32mAktivni  : $active_count \e[0m"
+echo -e "\e[33mNeaktivni: $inactive_count \e[0m"
+if [ $inactive_count -gt 0 ]; then
+    echo -e "Ineactiv devices:\n$inactive_ips"
+# na email - ne dela
+#    echo -e "Neaktivne naprave:\n$inactive_ips" | mailx -s "Neaktivne naprave">
+fi
