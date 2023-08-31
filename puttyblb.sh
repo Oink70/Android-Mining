dc55134666ba4e8c86392878a37459af4c1966ee
BLBMS <119702035+BLBMS@users.noreply.github.com>
2023-08-31 19:07:40 +0200

    Create puttyblb.sh

---
+++ puttyblb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ $# -eq 1 ]; then
+    choice_ip=$1
+else
+    read -r -n 3 -p "putty IP:" choice_ip
+fi
+putty -ssh blb@192.168.100.$choice_ip -P 8022 -geometry 80x40
+echo "  exit"
